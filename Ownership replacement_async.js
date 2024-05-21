1724c7cedb10df59d3be1ca1de746804b7257d5d
Jacky Li <jackyli5545@gmail.com>
2024-05-21 14:21:43 +0800

    Properly working async version

--- Ownership replacement_async.js
+++ Ownership replacement_async.js
@@ -1,99 +1,63 @@
-(() => { //main
+(() => {
+    //main
     'use strict';
     ///////const for Target APP
     const App_ID = 44; //Target App ID
     const staffID_fieldname = 'Lookup_0';
     ///////////get affected records
-    async function BulkUpdate(updateentryID_arr, RCV_ID) {
-        var body = {
-            'app': App_ID
-        };
-        var records = [];
-        for (var i in updateentryID_arr) {
-            var item = updateentryID_arr[i];
-            records.push({
-                "id": item,
-                "record": {
-                    [staffID_fieldname]: {
-                        "value": RCV_ID
-                    }
-                }
-            });
-        }
 
-        body.records = records;
-        console.log(JSON.stringify(body));
-        await kintone.api(kintone.api.url('/k/v1/records.json', true), 'PUT', body, function(commit_resp) {
+
+    kintone.events.on('app.record.create.submit', async(ev) => {
+        try {
+            var bulk_changeform = ev.record;
+            console.log(JSON.stringify(bulk_changeform));
+            var body = {
+                'app': App_ID,
+                'query': staffID_fieldname + "=" + bulk_changeform.Giver_ID.value, //Staff ID == Giver ID
+                'fields': ['$id', staffID_fieldname] // get record ID
+            };
+            const resp = await kintone.api(kintone.api.url('/k/v1/records.json', true), 'GET', body);
             // success
-            console.log(JSON.stringify(commit_resp));
-            console.log(commit_resp.records.length);
-            return commit_resp.records.length;
-        }, function(error) {
-            // error
-            console.log(JSON.stringify(error));
-            window.alert("Problem on swapping staff ID");
-            return -1;
-        });
-    }
-
-    kintone.events.on('app.record.create.submit', function(ev) {
-        var bulk_changeform = ev.record;
-        console.log(JSON.stringify(bulk_changeform));
-        var body = {
-            'app': App_ID,
-            'query': staffID_fieldname + "=" + bulk_changeform.Giver_ID.value, //Staff ID == Giver ID
-            'fields': ['$id', staffID_fieldname] // get record ID
-        };
-
-
-        kintone.api(kintone.api.url('/k/v1/records.json', true), 'GET', body, function(resp) {
-                // success
-                console.log(JSON.stringify(resp));
-                var updateentryID_arr = [];
-                for (let i = 0; i < resp.records.length; i++) {
-                    updateentryID_arr[i] = resp.records[i].$id.value;
-                }
-                console.table(updateentryID_arr);
-                ev.record.affected_num.value = BulkUpdate(updateentryID_arr, bulk_changeform.RCV_ID.value);
-                var body = {
-                    'app': App_ID
-                };
-                var records = [];
-                for (var i in updateentryID_arr) {
-                    var item = updateentryID_arr[i];
-                    records.push({
-                        "id": item,
-                        "record": {
-                            [staffID_fieldname]: {
-                                "value": RCV_ID
-                            }
+            console.log(JSON.stringify(resp));
+            if (resp.totalCount = 0) {
+                Console.log("No Sales Lead records related to the Giver staff ID!");
+                ev.record.affected_num.value = 0;
+                return ev;
+            }
+            var updateentryID_arr = [];
+            for (let i = 0; i < resp.records.length; i++) {
+                updateentryID_arr[i] = resp.records[i].$id.value;
+            }
+            var body = {
+                'app': App_ID
+            };
+            var modified_records = [];
+            for (var i in updateentryID_arr) {
+                var item = updateentryID_arr[i];
+                modified_records.push({
+                    "id": item,
+                    "record": {
+                        [staffID_fieldname]: {
+                            "value": bulk_changeform.RCV_ID.value
                         }
-                    });
-                }
-
-                body.records = records;
-                console.log(JSON.stringify(body));
-                kintone.api(kintone.api.url('/k/v1/records.json', true), 'PUT', body, function(commit_resp) {
-                    // success
-                    console.log(JSON.stringify(commit_resp));
-                    console.log(commit_resp.records.length);
-                    return commit_resp.records.length;
-                }, function(error) {
-                    // error
-                    console.log(JSON.stringify(error));
-                    window.alert("Problem on swapping staff ID");
-                    return -1;
+                    }
                 });
-            },
-            function(error) {
-                // error
-                console.log(JSON.stringify(error));
-                window.alert("Problem on looking up records");
+            }
+            body.records = modified_records;
 
-            });
-        console.log("returning EV");
-        console.log(JSON.stringify(ev));
+            const commit_resp = await kintone.api(kintone.api.url('/k/v1/records.json', true), 'PUT', body);
+            console.log(commit_resp);
+            if (commit_resp.records.length != resp.records.length) {
+                console.warn("Commit resp length does not match fetch resp length!")
+                console.warn(commit_resp.records)
+                ev.record.affected_num.value = commit_resp.records.length * -1;
+            } else ev.record.affected_num.value = commit_resp.records.length;
+            // success
+            console.log("returning EV");
+            console.log(JSON.stringify(ev));
+        } catch (err) {
+            console.error(err);
+        }
         return ev;
     });
-
 })();
